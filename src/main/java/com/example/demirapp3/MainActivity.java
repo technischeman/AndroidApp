72ac42ca63ead8304b0a3150ffc2870c3d13bce7
technischeman <79165779+technischeman@users.noreply.github.com>
2021-04-15 18:20:08 +0200

    Add files via upload

---
+++ src/main/java/com/example/demirapp3/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.demirapp3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
